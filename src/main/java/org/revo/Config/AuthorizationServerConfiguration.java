3cbbbb020b321e84479fab1cef6f927a7ec9fd62
ashraf <ashraf1abdelrasool@gmail.com>
2018-12-21 21:59:32 +0200

    push all

--- src/main/java/org/revo/Config/AuthorizationServerConfiguration.java
+++ src/main/java/org/revo/Config/AuthorizationServerConfiguration.java
@@ -49,6 +49,7 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
                 .authorizedGrantTypes("authorization_code")
                 .scopes("read", "write")
                 .autoApprove("read")
+                .redirectUris("https://asrevo.cfapps.io")
                 .secret(encoder.encode("revo"));
     }
 
