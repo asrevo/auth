d148e46a2e42f5a58589c17656be501fe0e8304f
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-04 19:43:14 +0200

    push all

--- src/main/java/org/revo/Config/SubjectAttributeUserTokenConverter.java
+++ src/main/java/org/revo/Config/SubjectAttributeUserTokenConverter.java
@@ -1,5 +1,7 @@
 package org.revo.Config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.revo.Domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
@@ -8,11 +10,17 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 class SubjectAttributeUserTokenConverter extends DefaultUserAuthenticationConverter {
+    ObjectMapper oMapper = new ObjectMapper();
+
     @Override
     public Map<String, ?> convertUserAuthentication(Authentication authentication) {
         Map<String, Object> response = new LinkedHashMap<>();
-        response.put("user", authentication.getPrincipal());
-        response.put(USERNAME, authentication.getName());
+        User principal = (User) authentication.getPrincipal();
+        Map<String, Object> map = oMapper.convertValue(principal, Map.class);
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (entry.getValue() != null) response.put(entry.getKey(), entry.getValue());
+        }
+        response.put("sub", authentication.getName());
         if (authentication.getAuthorities() != null && !authentication.getAuthorities().isEmpty()) {
             response.put(AUTHORITIES, AuthorityUtils.authorityListToSet(authentication.getAuthorities()));
         }
